3db5220546a5df2c0fd035fde21ef45966c16615
Paul Batchelor <thisispaulbatchelor@gmail.com>
2016-09-19 01:12:04 +0000

    audio playback on pi

--- tools/brian_browse/query.lua
+++ tools/brian_browse/query.lua
@@ -5,11 +5,13 @@ tmp = package.loadlib("tools/brian_browse/curses/ncurses.so", "luaopen_ncurses")
 curses = tmp()
 
 function quit(d)
+    if (d.page == 3) then return 1 end
     curses.endwin()
     return 0
 end
 
 function down(d)
+    if (d.page == 3) then return 1 end
     curses.mvprintw(d.pos, 1, " ")
     d.pos = math.min(d.pos + 1, d.size)
     curses.mvprintw(d.pos, 1, ">")
@@ -17,6 +19,7 @@ function down(d)
 end
 
 function up(d)
+    if (d.page == 3) then return 1 end
     curses.mvprintw(d.pos, 1, " ")
     d.pos = math.max(d.pos - 1, 1)
     curses.mvprintw(d.pos, 1, ">")
@@ -54,6 +57,7 @@ function get_sketch_name(id)
 end
 
 function print_page(d)
+    curses.clear()
     for i, row in pairs(d.rows) do
         curses.mvprintw(i, 3, row[2])
     end
@@ -62,10 +66,11 @@ function print_page(d)
 end
 
 function sel(d)
+    if (d.page ~= 1) then return 1 end
     curses.clear()
     local name = get_sketch_name(d.rows[d.pos][7])
     curses.attron(curses.A_BOLD())
-    curses.mvprintw(0, 0, name)
+    curses.mvprintw(0, 0, d.rows[d.pos][2])
     curses.attroff(curses.A_BOLD())
     local qstr = string.format("SELECT * from %s WHERE(id == %d);",
         name, d.rows[d.pos][8])
@@ -84,6 +89,8 @@ function sel(d)
     d.pos = 1
     curses.mvprintw(d.pos, 1, ">")
     d.page = 2
+
+    curses.mvprintw(d.size, 0, name)
     return 1
 end
 
@@ -96,16 +103,33 @@ function back(d)
 end
 
 function get_wav_file(pfx, tbl)
-    return string.format("%s%s_%s.wav", 
+    return string.format("%s%s-%s.wav", 
         pfx, tbl[2], tbl[3])
 end
 
 function play(d)
     if(d.page == 2) then
-        curses.clear()
-        curses.mvprintw(0, 0, get_wav_file(d.prefix, d.sketch))
+        cmd = string.format("aplay %s 2> /dev/null &", get_wav_file(d.prefix, d.sketch))
+        os.execute(cmd)
     end
     return 1
+end
+
+function stop(d)
+    os.execute("killall aplay 2> /dev/null")
+    return 1
+end
+
+function lock(d)
+    if(d.page == 3) then
+        d.page = 1
+        print_page(d)
+    else
+	d.page = 3
+        curses.clear()
+        curses.mvprintw(0, 0, "Screen is locked. Hit 'L' to unlock") 
+    end
+return 1
 end
 
 local out = query("SELECT * FROM brian ORDER BY id DESC limit 20")
@@ -123,6 +147,8 @@ tbl['k'] = up
 tbl['l'] = sel
 tbl['h'] = back
 tbl['p'] = play
+tbl['s'] = stop
+tbl['L'] = lock
 
 run = 1
 data = {}
@@ -132,7 +158,7 @@ data.size = 20
 data.page = 1
 data.top_pos = 1
 data.sketch = nil
-data.prefix = ""
+data.prefix = "../smp/brian/"
 
 print_page(data)
 
